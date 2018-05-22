d9a9503b6860290af4fec6317204add649556719
Daniel Konzalla <daniel.konzalla@student.uni-tuebingen.de>
2018-05-22 13:37:07 +0200

    elegantere Lösung für 3.2b)

--- Exercise03/2_demosaicing/bilinear.m
+++ Exercise03/2_demosaicing/bilinear.m
@@ -6,23 +6,13 @@ filter_rb = reshape(filter_rb,3,3);
 filter_g = [0,1,0,1,4,1,0,1,0]/4;
 filter_g = reshape(filter_g,3,3);
 
-linear = zeros(size(img));
-%loop through image without boundary pixels
-for i=2:767
-    for j=2:511
-        %set rgb to zero
-        red = 0;
-        green = 0;
-        blue = 0;
-        for m=-1:1
-            for n=-1:1
-                red = red + img(i+m,j+n,1)*filter_rb(m+2,n+2);
-                green = green + img(i+m,j+n,2)*filter_g(m+2,n+2);
-                blue = blue + img(i+m,j+n,3)*filter_rb(m+2,n+2);
-            end
-        end
-        linear(i,j,1) = red;
-        linear(i,j,2) = green;
-        linear(i,j,3) = blue;
-    end
-end
+%convolve each channel with the corresponding filter
+red = conv2(img(:,:,1), filter_rb);
+green = conv2(img(:,:,2),filter_g);
+blue = conv2(img(:,:,3),filter_rb);
+
+%concatenate all three color channels and crop the border of the image, so
+%the image has the same size as the input (important for calculating
+%linear_psnr)
+linear = cat(3,red, green, blue);
+linear = imcrop(linear, [2 2 511 767]);
