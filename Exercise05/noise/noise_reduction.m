a5327ce84b26440214926b69729d5fb259d836ff
Daniel Konzalla <daniel.konzalla@student.uni-tuebingen.de>
2018-06-01 12:05:22 +0200

    first try of first task and second task

--- Exercise05/noise/noise_reduction.m
+++ Exercise05/noise/noise_reduction.m
@@ -18,10 +18,35 @@ dark = zeros(size(images{1}));
 variance = zeros(size(images{1}));
 
 % compute the average dark frame image 'dark'
+a1 = 0;
+a2 = 0;
+a3 = 0;
+for n=1:nrDarkframes
+    a1 = a1 + darkframes{n}(:,:,1);
+    a2 = a2 + darkframes{n}(:,:,2);
+    a3 = a3 + darkframes{n}(:,:,3);
+end
 
+a1 = a1 ./ nrDarkframes;
+a2 = a2 ./ nrDarkframes;
+a3 = a3 ./ nrDarkframes;
 
+dark = cat(3,a1,a2,a3);
 % compute the average image 'average' and apply the dark frame 'dark' to it
+a1 = 0;
+a2 = 0;
+a3 = 0;
+for n=1:nrImages
+    a1 = a1 + images{n}(:,:,1);
+    a2 = a2 + images{n}(:,:,2);
+    a3 = a3 + images{n}(:,:,3);
+end
+a1 = a1 ./ nrDarkframes;
+a2 = a2 ./ nrDarkframes;
+a3 = a3 ./ nrDarkframes;
 
+average = cat(3,a1,a2,a3);
+average = average + dark;
 
 imgSize = size(average);
 blurImg = zeros(size(average));
@@ -31,7 +56,17 @@ gauss = fspecial('gaussian', 2*kernelSize+1, 1);
 
 % compute the convolution of the image 'images{1} with the kernel gauss
 % manually (no Matlab convolution functions ;))
-
+for i=2:size(images{1},1)-1
+    for j=2:size(images{1},2)-1
+        for c=1:3
+            for k1=-kernelSize:kernelSize
+                for k2 = -kernelSize:kernelSize
+                    blurImg(i,j,c) = blurImg(i,j,c) + gauss(k1+2, k2+2)*images{1}(i+k1,j+k2, c); 
+                end
+            end
+        end
+    end
+end
 
 % plot all (interesting) images
 ax(1)=subplot(2,2,1);
